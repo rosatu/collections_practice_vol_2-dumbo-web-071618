c9c5f76492b1ef06ff6f381053dc8a1aa5f03c01
Rosa Tu <rosalina.tu@gmail.com>
2018-07-13 20:49:52 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,11 +9,33 @@ def begins_with_r(tools)
 end    
 
 def contain_a(array)
-   array.map do |x|
-     if x.split.include? ("a") == false
-       binding.pry
-     array.pop(x)
+   a_array = []
+   array.each do |x|
+     if x.split("").include?("a") == true
+     a_array << x
      end
    end  
-   array
+   a_array
 end   
+
+def first_wa(array)
+  array.find do |element|
+  (element.class == String) && element.start_with?("wa")
+  end
+end  
+
+def remove_non_strings(array)
+   array.delete_if do |var|
+   var.class != String 
+   end
+  return array
+end    
+
+def count_elements(array)
+ array.uniq 
+ binding.pry
+  array.each do |element|
+    new_array << array.count(element)
+    end 
+  end  
+end  
