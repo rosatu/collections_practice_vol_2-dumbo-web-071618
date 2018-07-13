e91ecc726907581cfed0ecb04d3df19e34024d2e
Rosa Tu <rosalina.tu@gmail.com>
2018-07-13 19:05:40 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
-# your code goes here
+require 'pry'
+def begins_with_r(tools)
+  tools.any? do |x|
+  if x.start_with?("r") == false
+  return false
+  end
+  end
+  !!tools
+end    
+
+def contain_a(array)
+   array.map do |x|
+     if x.split.include? ("a") == false
+       binding.pry
+     array.pop(x)
+     end
+   end  
+   array
+end   
