a42c222532510b71c0e892a9eabfc7274731448d
Rosa Tu <rosalina.tu@gmail.com>
2018-07-14 20:27:28 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -70,6 +70,7 @@ def organize_schools (school_hash)
   school_hash.each do |container|
   container.each do |school, location_hash|
     location_hash.each do |location,location_value|
+      binding.pry
       new_location_hash[location_value] = "#{school}"
      end
    end
