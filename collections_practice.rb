2ce1cff266cfc599ab4b5b60a6c0f3bb9536c486
Rosa Tu <rosalina.tu@gmail.com>
2018-07-14 20:58:48 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -66,15 +66,15 @@ end
 end
 
 def organize_schools (school_hash)
-  binding.pry
   new_location_hash = {}
   location_array = []
   school_hash.each do |school_name, location_hash|
     location_hash.each do |location,location_value|
-    if school_name.values == location_value
+      if school_name.values == location_value
       location_array << school_name
-    new_location_hash[location_value] = location_array
-     end
-   end
+      new_location_hash[location_value] = location_array
+      end
+    end
+  end
  new_location_hash
 end 
